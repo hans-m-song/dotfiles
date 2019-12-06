8fa33a7973dafeb2bf7f81db5df760ee677bad36
Hans Song <hans.m.song@gmail.com>
2019-12-06 19:48:52 +1000

    fix path issue for merge

--- update.sh
+++ update.sh
@@ -2,17 +2,19 @@
 
 # merge left file ($1) INTO right file ($2)
 function merge () {
-    if [[ -f $2 ]]; then
-        sdiff -o $2 $1 $2
+    if [[ -f "$2" ]]; then
+        echo "merging $1 into $2"
+        sdiff -s -o "$2" "$1" "$2"
     else
-        cp $1 $2
+        echo "copying $1 to $2"
+        cp "$1" "$2"
     fi
 }
 
 merge .zshrc ~/.zshrc
 merge .gitconfig ~/.gitconfig
 merge .vimrc ~/.vimrc
-merge custom.zsh $ZSH_CUSTOM/custom.zsh
+merge custom.zsh $ZSH/custom/custom.zsh
 
 case "$OSTYPE" in
     darwin*)
