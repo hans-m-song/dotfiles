3ac7400f4b8c0910e3024987633be730d896ef9d
Hans Song <hans.m.song@gmail.com>
2023-06-13 21:40:13 +1000

    add nvm

--- .zshrc
+++ .zshrc
@@ -1,9 +1,8 @@
 export ZSH=$HOME/.oh-my-zsh
-export NVM_DIR="$HOME/.nvm"
 export EDITOR=vim
 
 ZSH_THEME="powerlevel10k/powerlevel10k"
-# zstyle :omz:plugins:ssh-agent identities id_rsa id_ed25519
+# zstyle :omz:plugins:ssh-agent identities id_rsa
 plugins=(aws gh git ssh-agent kubectl)
 source $ZSH/oh-my-zsh.sh
 autoload -U add-zsh-hook
@@ -15,7 +14,11 @@ eval "$(direnv hook zsh)"
 
 # nvm
 load-nvmrc() {
-  test -f .nvmrc && command -v nvm >/dev/null && nvm use
+  test -f .nvmrc &&
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" &&
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" &&
+    command -v nvm >/dev/null &&
+    nvm use
 }
 
 add-zsh-hook chpwd load-nvmrc
--- setup-shell.sh
+++ setup-shell.sh
@@ -9,6 +9,7 @@ brew install \
   direnv \
   gh \
   git-secret \
+  gnupg \
   helmfile \
   jq \
   k9s \
@@ -27,11 +28,14 @@ brew install \
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-cp ./.zshrc ~/.zshrc
-cp ./.vimrc ~/.vimrc
-cp -R ./.vim ~/.vim
-
 # powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sed -i -E 's|ZSH_THEME=".*"|ZSH_THEME="powerlevel10k/powerlevel10k"|' ~/.zshrc
+
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+cp ./.zshrc ~/.zshrc
+cp ./.vimrc ~/.vimrc
+cp -R ./.vim ~/.vim
 source ~/.zshrc
